0b441782507a6fd8fdacbf54a2487beb63b45bdf
노영선 <dudtjs303@yonsei.ac.kr>
2023-01-02 14:55:09 +0900

    노드개수

--- AI/keras/keras04_deep.py
+++ AI/keras/keras04_deep.py
@@ -7,10 +7,10 @@ x = np.array([1,2,3,4,5])
 y = np.array([1,2,3,4,5])
 
 model = Sequential()
-model.add(Dense(3, input_dim = 1)) # input layer를 제외하고는 input을 입력하지 않아도 됨
-model.add(Dense(9)) 
-model.add(Dense(7))
-model.add(Dense(4)) # hidden layer
+model.add(Dense(10, input_dim = 1)) # input layer를 제외하고는 input을 입력하지 않아도 됨
+model.add(Dense(8)) 
+model.add(Dense(6))
+model.add(Dense(2)) # hidden layer
 model.add(Dense(1)) # output layer
 
 model.compile(loss = 'mae', optimizer = 'Adam')
