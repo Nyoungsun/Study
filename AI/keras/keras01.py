218f63d45974c8d649acbf68f5f0a34b20535297
노영선 <dudtjs303@yonsei.ac.kr>
2023-01-02 10:33:11 +0900

    복습

--- AI/keras/keras01.py
+++ AI/keras/keras01.py
@@ -10,10 +10,10 @@ from tensorflow.keras.models import Sequential #순차적 모델
 from tensorflow.keras.layers import Dense      #모델 구성 방법
 
 model = Sequential()
-model.add(Dense(1, input_dim=1)) #1 = y(output_dim) - 1차원 배열
+model.add(Dense(1, input_dim=1)) #1 = y(output_dim) → 1차원 배열
 
 #3. 컴파일, 훈련
-model.compile(loss='mae', optimizer='adam') #mae = minimum average error, optimizer = loss 최적화
+model.compile(loss='mae', optimizer='adam') #mae = mean absolute error, optimizer → loss 최적화
 model.fit(x, y, epochs=2000) #epochs = 훈련 횟수
 
 #4. 평가, 예측
