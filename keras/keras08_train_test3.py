e8755f78198b1766bbd06f6663e1cbc09853a4e3
노영선 <dudtjs303@yonsei.ac.kr>
2023-01-03 17:12:51 +0900

    second

--- keras/keras08_train_test3.py
+++ keras/keras08_train_test3.py
@@ -1,25 +1,28 @@
-import numpy as np
+import numpy as np 
+from sklearn.model_selection import train_test_split
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 
 x = np.array([1,2,3,4,5,6,7,8,9,10]) 
 y = np.array(range(10))
 
-x_train = x[:7] # 1,2,3,4,5,6,7
-x_test = x[7:] # 8,9,10
-
-y_train = y[:7] # = [:-2] → 0,1,2,3,4,5,6 
-y_test = y[7:] # = [-3:] → 7,8,9 
+x_train, x_test, y_train, y_test = train_test_split(
+    x, y, 
+    test_size=0.3,
+    train_size=0.7,     #train, test 중 하나만 지정해도됨
+    shuffle=True,       #default = True
+    random_state=140     #random_state 지정 = random seed 고정
+    )
 
 model = Sequential()
-model.add(Dense(10, input_dim = 1))
+model.add(Dense(20, input_dim = 1))
 model.add(Dense(30))
 model.add(Dense(20))
 model.add(Dense(10))
 model.add(Dense(1))
 
 model.compile(loss='mae', optimizer = 'adam')
-model.fit(x_train, y_train, epochs=200, batch_size=1) # 훈련 데이터로만 훈련해야함
+model.fit(x_train, y_train, epochs=300, batch_size=1) # 훈련 데이터로만 훈련해야함
 
 print('\n')
 loss = model.evaluate(x_test, y_test) # 평가 데이터로만 평가해야함 (훈련 데이터 범위 내에서 평가 데이터를 분리)
