66ddd3ceafbdfd3197f5bfec1c54928f49753751
노영선 <dudtjs303@yonsei.ac.kr>
2023-01-09 18:15:31 +0900

    0109

--- keras/keras18_overfit1_boston.py
+++ keras/keras18_overfit1_boston.py
@@ -5,8 +5,10 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib import font_manager, rc
 
-#1. 데이터
+
+# 1. 데이터
 dataset = load_boston()         # 보스턴 집 값에 대한 데이터
 x = dataset.data                # 방 넓이, 방 개수 등 → 독립변수
 y = dataset.target              # 집 값 → 종속변수
@@ -22,9 +24,9 @@ x_train, x_test, y_train, y_test = train_test_split(
     x, y, train_size=0.7, random_state=3333
 )
 
-#2. 모델구성
+# 2. 모델구성
 model = Sequential()
-model.add(Dense(32, input_shape = (13,)))
+model.add(Dense(32, input_shape=(13,)))
 model.add(Dense(64))
 model.add(Dense(256))
 model.add(Dense(512))
@@ -32,11 +34,13 @@ model.add(Dense(128))
 model.add(Dense(1))
 
 
-#3. 컴파일 및 훈련
-model.compile(loss = 'mse', optimizer='adam')
-hist = model.fit(x_train, y_train, epochs=200, batch_size=1, validation_split=0.2, verbose = 3) #verbose: 함수 수행시 발생하는 상세한 정보들을 표준 출력으로 자세히 내보낼 것인지
+# 3. 컴파일 및 훈련
+model.compile(loss='mse', optimizer='adam')
+# verbose: 함수 수행시 발생하는 상세한 정보들을 표준 출력으로 자세히 내보낼 것인지
+hist = model.fit(x_train, y_train, epochs=1, batch_size=1,
+                 validation_split=0.2, verbose=3)
 
-#4. 평가 및 예측
+# 4. 평가 및 예측
 loss = model.evaluate(x_test, y_test, verbose=3)
 y_predict = model.predict(x_test)
 # print('x_test:\n', x_test)
@@ -44,7 +48,7 @@ y_predict = model.predict(x_test)
 print('loss: ', loss)
 
 # print(hist) # <keras.callbacks.History object at 0x000001ECB4986D00>
-# print(hist.history) # 딕셔너리(key, value) → loss의 변화값을 list로(value는 list로 저장된다.)  
+# print(hist.history) # 딕셔너리(key, value) → loss의 변화값을 list로(value는 list로 저장된다.)
 # print(hist.history['loss']) # key = loss인 것만 출력
 
 RMSE = np.sqrt(mean_squared_error(y_test, y_predict))
@@ -53,12 +57,16 @@ r2 = r2_score(y_test, y_predict)
 print("R2: ", r2)
 
 # --------------------- 시각화 ----------------------- #
-plt.figure(figsize=(9,6))
-plt.plot(hist.history['loss'], c='red', marker='.', label = 'loss')
-plt.plot(hist.history['val_loss'], c='blue', marker='.', label = 'val_loss')
-plt.grid() 
+plt.figure(figsize=(9, 6))
+plt.plot(hist.history['loss'], c='red', marker='.', label='loss')
+plt.plot(hist.history['val_loss'], c='blue', marker='.', label='val_loss')
+plt.grid()
 plt.xlabel('epochs')
 plt.ylabel('loss')
-plt.legend() # label 출력 # plt.legend(loc = 'upper left')
-plt.title("boston loss")
+plt.legend()  # label 출력 # plt.legend(loc = 'upper left')
+font_path = "C:/Windows/Fonts/malgun.ttf"
+plt.rc('font', family='malgun')
+font = font_manager.FontProperties(fname=font_path).get_name()
+rc('font', family=font)
+plt.title("보스턴")
 plt.show()
--- keras/keras20_sigmoid_cancer.py
+++ keras/keras20_sigmoid_cancer.py
@@ -1,11 +1,10 @@
 from sklearn.datasets import load_breast_cancer
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.metrics import accuracy_score
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
-from tensorflow.keras.callbacks import EarlyStopping 
-import numpy as np
-import matplotlib.pyplot as plt
+from tensorflow.keras.callbacks import EarlyStopping
+import numpy as np 
 
 #1. 데이터
 dataset = load_breast_cancer()        
@@ -35,11 +34,20 @@ model.add(Dense(1, activation='sigmoid'))
 #3. 컴파일 및 훈련
 model.compile(loss = 'binary_crossentropy', optimizer='adam', metrics=['accuracy']) # 이진분류 loss = binary_crossentropy
 earlyStopping = EarlyStopping(monitor='val_loss', mode='min', patience=20, restore_best_weights=True) # loss - min, accuracy - max 
-model.fit(x_train, y_train, epochs=1000, batch_size=16, validation_split=0.2, callbacks = [earlyStopping], verbose = 2) # verbose: 함수 수행시 발생하는 상세한 정보들을 표준 출력으로 자세히 내보낼 것인지
+# earlyStopping = EarlyStopping(monitor='accuracy', mode='max', patience=20, restore_best_weights=True) 
+hist = model.fit(x_train, y_train, epochs=1000, batch_size=10, validation_split=0.2, callbacks = [earlyStopping], verbose = 2) # verbose: 함수 수행시 발생하는 상세한 정보들을 표준 출력으로 자세히 내보낼 것인지
 
 #4. 평가 및 예측
 loss, accuracy = model.evaluate(x_test, y_test)
+print('loss: ', loss, 'accuracy: ', accuracy)
+
 y_predict = model.predict(x_test)
-# print('x_test:\n', x_test)
-# print('y_predict:\n', y_predict)
-print('loss: ', loss , 'accuracy: ', accuracy) 
+y_predict = y_predict.flatten()
+y_predict = np.where(y_predict > 0.5, 1, 0)
+print('y_predict: ', y_predict[:10], 'y_test: ', y_test[:10])
+
+acc = accuracy_score(y_test, y_predict)
+print('acc: ', acc)
+
+# print(hist.history['val_loss']) # metrics를 넣으면 history에 metrics에 대한 수치도 나온다.
+# print(hist.history['accuracy'])
