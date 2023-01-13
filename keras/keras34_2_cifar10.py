f8f43f0893d5aade70be3a2c28d5dc21a11733e7
노영선 <dudtjs303@yonsei.ac.kr>
2023-01-13 19:06:21 +0900

    0113

--- keras/keras34_2_cifar10.py
+++ keras/keras34_2_cifar10.py
@@ -1,6 +1,6 @@
 from tensorflow.keras.datasets import cifar10
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D, Dense, Flatten
+from tensorflow.keras.layers import Conv2D, Dense, Flatten, Dropout
 from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
 from sklearn.preprocessing import MinMaxScaler as MMS, StandardScaler as SDS
 import matplotlib.pyplot as plt
@@ -22,11 +22,15 @@ path = 'C:/study/keras_save/MCP/'
 #2. 모델
 model = Sequential()
 model.add(Conv2D(filters=128, kernel_size = (2,2), input_shape=(32,32,3), activation='relu'))
+model.add(Dropout(0.5)) 
 model.add(Conv2D(filters=64, kernel_size=(2,2))) # input_size = (27, 27, 128)
+model.add(Dropout(0.3)) 
 model.add(Conv2D(filters=64, kernel_size=(2,2))) # input_size = (26, 26, 64)
 model.add(Flatten())
 model.add(Dense(units=64, activation='relu'))
+model.add(Dropout(0.3)) 
 model.add(Dense(units=32, activation='relu'))
+model.add(Dropout(0.2)) 
 model.add(Dense(units=16, activation='relu'))
 model.add(Dense(units=10, activation='softmax'))
 
@@ -35,7 +39,7 @@ model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=
 
 MCP = ModelCheckpoint(monitor='val_loss', mode = 'auto', save_best_only=True, filepath = path + 'keras34_2_cifar10.hdf5') 
 ES = EarlyStopping(monitor = 'val_loss', mode = min, patience=4, restore_best_weights = True) 
-model.fit(x_train, y_train, epochs=32, batch_size=1024, validation_split=0.2, callbacks=[ES, MCP])
+model.fit(x_train, y_train, epochs=64, batch_size=1024, validation_split=0.2, callbacks=[ES, MCP])
 
 #4. 평가 및 예측
 metric = model.evaluate(x_test, y_test) # compile에서 metrics = acc를 지정했으므로 evaluate는 값을 배열 형태로 2개 반환함
