43c782acd16052e3c32e7e27027c04bea810dcbc
노영선 <dudtjs303@yonsei.ac.kr>
2023-01-14 19:58:18 +0900

    0114

--- web/app.js
+++ web/app.js
@@ -1,9 +1,17 @@
 // const: 변경 X, let: 변경 O
+// 단어 한번에 변경: ctrl + shift + L
+// 괄호 한번에 처리: 드래그 후 괄호
 
-const mon = "mon";
-const tue = "tue";
-const wed = "wed";
-const thu = "thu";
+const daysOfWeek = ["mon", "tue", "wed", "thu"]
 
-const dayOfWeek = mon + tue + wed + thu;
-console.log(dayOfWeek);
+
+//get item from array
+console.log(daysOfWeek)
+console.log(daysOfWeek[2])
+
+// Add one more day to array
+daysOfWeek.push("fri")
+
+console.log(daysOfWeek)
+
+
