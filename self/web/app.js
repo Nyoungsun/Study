7c41de53c52756f1bfb4a5b80217c249b784b0d2
노영선 <dudtjs303@yonsei.ac.kr>
2023-01-21 19:02:17 +0900

    recap1

--- self/web/app.js
+++ self/web/app.js
@@ -1,20 +1,26 @@
-function plus(a, b) {
-    console.log(a + b);
-}
+// function plus(a, b) {
+//     console.log(a + b);
+// }
 
-function devide(aa, bb) {
-    console.log(aa / bb);
-}
+// function devide(aa, bb) {
+//     console.log(aa / bb);
+// }
 
-plus(8, 60);
-devide(8, 60);
+// plus(8, 60);
+// devide(8, 60);
+ 
+// const player = {
+//     name: "nao",
+//     sayHello: function(other){
+//         console.log("hello! " + other);
+//     }
+// };
 
-const player = {
-    name: "nao",
-    sayHello: function(other){
-        console.log("hello! " + other);
-    }
-};
+// console.log(player.name);
+// player.sayHello("didi");
 
-console.log(player.name);
-player.sayHello("didi");
+// const tobuy = ["A", "B", "C"];
+// tobuy[2]="D";
+// console.log(tobuy);
+// tobuy.push("E");
+// console.log(tobuy);
