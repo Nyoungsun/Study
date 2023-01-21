114afb5740ebcb42cb6fb1716e0f091f99706bed
노영선 <dudtjs303@yonsei.ac.kr>
2023-01-21 20:59:19 +0900

    return

--- self/web/app.js
+++ self/web/app.js
@@ -6,6 +6,20 @@
 //     console.log(aa / bb);
 // }
 
+// function mul() {
+//     console.log(2 * 2);
+// }
+// mul();
+// mul();
+// mul();
+
+// function add(a) {
+//     console.log(a-5);
+// }
+
+// add(5, 10, 11, 12); //첫번째 인자만 받음
+
+
 // plus(8, 60);
 // devide(8, 60);
  
@@ -16,6 +30,12 @@
 //     }
 // };
 
+// console.log(player, console);
+// player.name="voa";
+// console.log(player);
+// player.age = 88;
+// console.log(player);
+
 // console.log(player.name);
 // player.sayHello("didi");
 
@@ -24,3 +44,42 @@
 // console.log(tobuy);
 // tobuy.push("E");
 // console.log(tobuy);
+
+const calc = {
+    add: function(a, b) {
+        return a+b;
+    },
+
+    sub: function(a,b) {
+        return a-b;
+    },
+
+    div: function(a,b) {
+        return a/b;
+    },
+
+    mul: function(a,b) {
+        return a*b;
+    },
+
+    power: function(a,b) {
+        return a**b;
+    }
+};
+
+const add = calc.add(1,2);
+const sub = calc.sub(add,2);
+const div =calc.div(sub,2);
+const mul = calc.mul(div,2);
+const power = calc.power(mul,2);
+
+console.log(add, sub, div, mul, power);
+
+
+// const age = 96;
+// function cal(age){
+//     return age + 2;
+// }
+
+// const age2 = cal(age);
+// console.log(age2);
