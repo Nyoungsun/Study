3044127efcb1e6fe57450c75326cb5c598b7479c
노영선 <dudtjs303@yonsei.ac.kr>
2023-01-27 16:37:36 +0900

    0127

--- keras/keras32_cnn.py
+++ keras/keras32_cnn.py
@@ -5,6 +5,7 @@ model = Sequential()
 # ---------- CNN ---------- #                                        
 model.add(Conv2D(filters=10, kernel_size=(2,2), input_shape=(5,5,1)))  # ex: image = (60000, 5, 5, 1)               
 model.add(Conv2D(5, (2,2)))                                            # input_shape = (N, 4, 4, 10) → 행 무시, 열 우선
+model.add(Conv2D(5, (2)))                                              # kernel_size = 2 => (2, 2)와 같음
 # ---------- DNN ---------- #
 model.add(Flatten())                                                   # input_shape = (N, 3, 3, 5) output_shape = (N, 45) = (45, ) → 행 무시, 열 우선, 
 model.add(Dense(units = 10))                                           # (N, 10)
