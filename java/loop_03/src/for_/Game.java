f951581ba7c42952b5eabfb792c0787ba2187489
노영선 <dudtjs303@yonsei.ac.kr>
2023-02-04 14:44:51 +0900

    0204

--- java/loop_03/src/for_/Game.java
+++ java/loop_03/src/for_/Game.java
@@ -7,55 +7,63 @@ public class Game {
         int coin, balance, com;
         char enter;
         String result;
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Insert coin(at least 300):  ");
+            coin = scanner.nextInt();
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Insert coin(최소 300원):  ");
-        coin = scanner.nextInt();
+            for (balance = coin; balance >= 300; ) {
+                System.out.print("Enter a number. [1: Rock, 2: Scissor, 3: Paper] ▶ ");
+                int user = scanner.nextInt();
+                if (user > 3 || user < 1) {
+                    System.out.println("Enter a number between 1 and 3.");
+                    continue;
+                } else {
+                    com = (int) ((Math.random() * 3) + 1);
+                    System.out.printf("computer: %s, user: %s\n",
+                            (com == 1 ? "Scissor" : com == 2 ? "Rock" : "Paper"),
+                            (user == 1 ? "Scissor" : user == 2 ? "Rock" : "Paper"));
+                    if (com == 1) {
+                        if (user == 1) {
+                            result = "Draw";
+                        } else if (user == 2) {
+                            result = "You win";
+                        } else {
+                            result = "You lose";
+                        }
+                        System.out.println(result);
+                    }
+                    if (com == 2) {
+                        if (user == 1) {
+                            result = "You lose";
+                        } else if (user == 2) {
+                            result = "Draw";
+                        } else {
+                            result = "You win";
+                        }
+                        System.out.println(result);
+                    }
+                    if (com == 3) {
+                        if (user == 1) {
+                            result = "You win";
+                        } else if (user == 2) {
+                            result = "You lose";
+                        } else {
+                            result = "Draw";
+                        }
+                        System.out.println(result);
+                    }
 
-        for (balance = coin; balance >= 300; ) {
-            System.out.print("가위(1), 바위(2), 보자기(3) 중 번호 입력: ");
-            int user = scanner.nextInt();
-            if (user > 3 || user < 1) {
-                System.out.println("1 ~ 3 사이의 숫자만 입력하세요");
-                continue;
-            } else {
-                com = (int) ((Math.random() * 3) + 1);
-                System.out.printf("computer: %s, user: %s\n",
-                        (com == 1 ? "가위" : com == 2 ? "바위" : "보자기"),
-                        (user == 1 ? "가위" : user == 2 ? "바위" : "보자기"));
-                if (com == 1) {
-                    if (user == 1) {
-                        result = "Draw";
-                    } else if (user == 2) {
-                        result = "You win";
-                    } else {
-                        result = "You lose";
-                    }
-                    System.out.println(result);
                 }
-                if (com == 2) {
-                    if (user == 1) {
-                        result = "You lose";
-                    } else if (user == 2) {
-                        result = "Draw";
-                    } else {
-                        result = "You win";
-                    }
-                    System.out.println(result);
-                }
-                if (com == 3) {
-                    if (user == 1) {
-                        result = "You win";
-                    } else if (user == 2) {
-                        result = "You lose";
-                    } else {
-                        result = "Draw";
-                    }
-                    System.out.println(result);
-                }
-
+                balance -= 300;
             }
-            balance -= 300;
+            System.out.println("Game Over. [exit: 0, continue:1] ▶ ");
+            int input = scanner.nextInt();
+            if (input == 0) {
+                break;
+            } else if (input == 1) {
+                continue;
+            }
         }
     }
 }
