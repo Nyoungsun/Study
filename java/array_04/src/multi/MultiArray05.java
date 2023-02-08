4af34571446abc448cbfb785761574db074ced31
노영선 <dudtjs303@yonsei.ac.kr>
2023-02-08 21:27:29 +0900

    0208

--- java/array_04/src/multi/MultiArray05.java
+++ java/array_04/src/multi/MultiArray05.java
@@ -1,10 +1,43 @@
 package multi;
 
+import java.util.Scanner;
+
 public class MultiArray05 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        String[] subject;
+        String name;
+        int cnt, subjectCnt, total;
+        float avg;
+        int[] score;
+
+        System.out.print("인원수 입력: ");
+        cnt = scanner.nextInt();
+
+        for (int i = 0; i < cnt; i++) {
+            System.out.print("이름입력: ");
+            name = scanner.next();
+
+            System.out.print("과목수 입력: ");
+            subjectCnt = scanner.nextInt();
+            subject = new String[subjectCnt];
+
+            for (int j = 0; j < subject.length; j++) {
+                System.out.print("과목명 입력: ");
+                subject[j] = scanner.next();
+            }
+
+            for (int j = 0; j < subject.length; j++) {
+                System.out.printf("%s 점수 입력: ", subject[j]);
+                score = new int[subjectCnt];
+                score[j] = scanner.nextInt();
+            }
+            System.out.println("-------------------------");
+        }
     }
 }
+
 
 /*
 인원수를 입력하여 인원수만큼 데이터를 입력받고 총점과 평균을 구하시오
