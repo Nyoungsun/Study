83185034b9666ee3308177b274a19a263c517b21
노영선 <dudtjs303@yonsei.ac.kr>
2023-02-08 11:16:20 +0900

    0208

--- java/array_04/src/array/BaseBall.java
+++ java/array_04/src/array/BaseBall.java
@@ -5,72 +5,81 @@ import java.util.Scanner;
 public class BaseBall {
     public static void main(String[] args) {
 
-        int strike = 0;
-        int ball = 0;
         int[] random = new int[3];
         int[] input = new int[3];
+        String user;
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
+            int strike = 0;
+            int ball = 0;
+
             System.out.print("게임을 실행하시겠습니까? (Y/N): ");
             char start = scanner.next().charAt(0);
 
             if (start == 'y' || start == 'Y') {
-                System.out.println("------------------------------");
-                System.out.println("Game Start");
-                System.out.println("------------------------------");
+                System.out.println("==== Game Start ====");
+                // 난수 발생
                 for (int i = 0; i < random.length; i++) {
                     random[i] = (int) ((Math.random() * 9) + 1);
+                    // 중복 제거
                     for (int j = 0; j < i; j++) {
-                        if ((random[i] == random[j])) {
+                        if (random[i] == random[j]) {
                             i--;
-                            break; // Back 21 line
+                            break;
                         }
-                    }
+                    } // for j
+                } // for i
+
+                // 난수 확인
+                System.out.print("힌트: ");
+                for (int x : random) {
+                    System.out.print(x);
                 }
-//                for (int x : random) {
-//                    System.out.print(x);
-//                }
-//                System.out.println();
+                System.out.println();
+
                 while (true) {
-                    System.out.print("숫자 입력 (space bar 구분): ");
+                    System.out.print("중복되지 않게 숫자 입력: ");
+                    user = scanner.next();
                     for (int i = 0; i < input.length; i++) {
-                        input[i] = scanner.nextInt();
-                        for (int j = 0; j < i; j++) {
-                            if ((input[i] == input[j])) {
-                                System.out.println("중복 값이 있습니다. 다시 입력하세요.");
-                                i--;
-                                break;
-                            }
-                        }
+                        input[i] = user.charAt(i) - 48;
                     }
-//                    for (int y : input) {
-//                        System.out.println(y);
-//                    }
-//                System.out.println();
+
+                    // 입력값 확인
+                    for (int y : input) {
+                        System.out.print(y);
+                    }
+
+                    System.out.println();
                     for (int i = 0; i < random.length; i++) {
                         for (int j = 0; j < input.length; j++) {
                             if (random[i] == input[j] && i == j) {
                                 strike++;
-                            } else if (random[i] == input[j] && i != j) {
+                            } else if (random[i] == input[i] && i != j) {
                                 ball++;
                             }
                         }
                     }
-                    System.out.printf("%d스트라이크, %d볼\n", strike, ball);
-
+                    System.out.printf("%d 스트라이크, %d 볼\n", strike, ball);
                     if (strike == 3) {
                         break;
                     }
                 }
                 strike = 0;
                 ball = 0;
+
             } else if (start == 'n' || start == 'N') {
+                System.out.println("프로그램을 종료합니다.");
                 break;
             }
         }
     }
 }
+
+
+
+
+
 
 /*
 [문제] 야구게임
