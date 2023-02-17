5e9c77fafd053f93f3ab64cbaceceb8a0d68d11e
노영선 <dudtjs303@yonsei.ac.kr>
2023-02-17 16:05:13 +0900

    0217

--- java/io_12/src/member/FileRead.java
+++ java/io_12/src/member/FileRead.java
@@ -1,18 +1,17 @@
 package member;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
+import java.io.*;
 import java.util.List;
 
 public class FileRead implements Member {
     @Override
     public void execute(List<MemberDTO> memberDTOs) throws IOException {
-        ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream("member.txt"));
         try {
-            System.out.println("이름\t\t나이\t\tPhone\t\tAddress");
+            ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream("member.txt"));
 
-            for (int i = 0; i < memberDTOs.size(); i++) {
+            System.out.println("이름\t\t나이\t\tPhone\t\tAddress");
+            int i;
+            for (i = 0; i < memberDTOs.size(); i++) {
                 MemberDTO data = (MemberDTO) objectInputStream.readObject();
                 System.out.printf("%s\t%d\t\t%s\t%s\n",
                         data.getName(),
@@ -20,6 +19,8 @@ public class FileRead implements Member {
                         data.getPhone(),
                         data.getAddress());
             }
+        } catch (FileNotFoundException e) {
+            System.out.println("파일을 찾을 수 없습니다.");
         } catch (ClassNotFoundException e) {
             throw new RuntimeException(e);
         }
